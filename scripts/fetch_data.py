f820b2c3485ce8e7b807e3373cac30fdb4265ce0
Matt Butterfield <mbutterfield@onshift.com>
2014-05-10 17:52:14 -0400

    clean data

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -14,7 +14,7 @@ def main():
         if parking_space[28] == 'COMPLETE' and parking_space[29] == 'INSTALLED':
             filtered_data.append({
                 'id': parking_space[0],
-                'name': '"%s"' % parking_space[8].replace('"', ''),
+                'name': parking_space[8],
                 'lat': parking_space[20][1],
                 'lng': parking_space[20][2],
             })
@@ -22,7 +22,7 @@ def main():
         parking_file.write('id, name, lat, lng\n')
         for parking_space in filtered_data:
             row = u"%s, %s, %s, %s\n" % (parking_space['id'],
-                                         parking_space['name'],
+                                         parking_space['name'].replace(",", ""),
                                          parking_space['lat'],
                                          parking_space['lng'])
             parking_file.write(row)
