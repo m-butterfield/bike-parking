5235e5b03728ce513d413fc0d0687559983a0b69
Matt Butterfield <mbutterfield@onshift.com>
2014-05-10 14:18:30 -0400

    build query to get parking results

--- bike_parking/static/js/views/starting_search_form.js
+++ bike_parking/static/js/views/starting_search_form.js
@@ -9,6 +9,7 @@ define([
         initialize: function() {
             this.listenTo(BikeParking.boxVent, 'box:places_changed', this.updateStartPoint);
             this.listenTo(BikeParking.mapVent, 'map:bounds_changed', this.updateSearchBounds);
+            //this.listenTo(BikeParking.mapVent, 'map:bounds_changed', this.fetchParkingResults);
             this.startIcon = {
                 url: '/static/img/blue-bike.gif',
                 size: new google.maps.Size(70, 70),
@@ -16,7 +17,13 @@ define([
                 anchor: new google.maps.Point(35, 70),
                 scaledSize: new google.maps.Size(70, 70)
             };
-            this.startMarker = undefined;
+            this.parkingIcon = {
+                url: '/static/img/green-bike.gif',
+                size: new google.maps.Size(70, 70),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(35, 70),
+                scaledSize: new google.maps.Size(70, 70)
+            };
         },
 
         updateSearchBounds: _.throttle(function() {
@@ -46,7 +53,27 @@ define([
                 position: startingPoint
             });
 
-        }
+            var bounds = new google.maps.LatLngBounds();
+            bounds.extend(startingPoint);
+            BikeParking.map.fitBounds(bounds);
+            BikeParking.map.setZoom(17);
+            this.fetchParkingResults()
+        },
+
+        fetchParkingResults: _.throttle(function() {
+            var bounds = BikeParking.map.getBounds();
+            var query = "?$where=within_box(coordinates, " +
+                bounds.getNorthEast().lat() + "," +
+                bounds.getSouthWest().lng() + "," +
+                bounds.getSouthWest().lat() + "," +
+                bounds.getNorthEast().lng() + ")" +
+                "AND status = 'COMPLETE' AND status_detail = 'INSTALLED'";
+            $.ajax({
+                url: BikeParking.API_ENDPOINT + query
+            }).done(function(results) {
+                debugger;
+            });
+        }, 1000)
 
     });
 
