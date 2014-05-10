107bf4474f5fe68a259bb5ef2a764a535f43358f
Matt Butterfield <mbutterfield@onshift.com>
2014-05-10 15:25:37 -0400

    show parking locations on map

--- bike_parking/static/js/views/starting_search_form.js
+++ bike_parking/static/js/views/starting_search_form.js
@@ -1,6 +1,21 @@
 define([
     'backbone'
 ], function(Backbone) {
+
+    var ParkingResult = Backbone.Model.extend({
+        initialize: function(options) {
+            this.latitude = options.latitude;
+            this.longitude = options.longitude;
+            this.location = options.location;
+            this.name = options.name;
+            this.marker = options.marker;
+        }
+    });
+
+    var ParkingResults = Backbone.Collection.extend({
+        model: ParkingResult
+    });
+
     var StartingSearchForm = Backbone.View.extend({
         events: {
             'submit': 'handleSubmit'
@@ -9,7 +24,7 @@ define([
         initialize: function() {
             this.listenTo(BikeParking.boxVent, 'box:places_changed', this.updateStartPoint);
             this.listenTo(BikeParking.mapVent, 'map:bounds_changed', this.updateSearchBounds);
-            //this.listenTo(BikeParking.mapVent, 'map:bounds_changed', this.fetchParkingResults);
+            this.listenTo(BikeParking.mapVent, 'map:idle', this.fetchParkingResults);
             this.startIcon = {
                 url: '/static/img/blue-bike.gif',
                 size: new google.maps.Size(70, 70),
@@ -24,6 +39,7 @@ define([
                 anchor: new google.maps.Point(35, 70),
                 scaledSize: new google.maps.Size(70, 70)
             };
+            this.parkingResults = new ParkingResults();
         },
 
         updateSearchBounds: _.throttle(function() {
@@ -57,21 +73,74 @@ define([
             bounds.extend(startingPoint);
             BikeParking.map.fitBounds(bounds);
             BikeParking.map.setZoom(17);
-            this.fetchParkingResults()
         },
 
         fetchParkingResults: _.throttle(function() {
             var bounds = BikeParking.map.getBounds();
-            var query = "?$where=within_box(coordinates, " +
+            var query = "?$limit=200&$where=within_box(coordinates, " +
                 bounds.getNorthEast().lat() + "," +
                 bounds.getSouthWest().lng() + "," +
                 bounds.getSouthWest().lat() + "," +
                 bounds.getNorthEast().lng() + ")" +
-                "AND status = 'COMPLETE' AND status_detail = 'INSTALLED'";
+                "AND status='COMPLETE' AND status_detail='INSTALLED'";
+            var that = this;
             $.ajax({
                 url: BikeParking.API_ENDPOINT + query
             }).done(function(results) {
-                debugger;
+                // if none were found, remove all from the map and collection
+                if (!results) {
+                    that.parkingResults.each(function(parkingResult) {
+                        parkingResult.marker.setMap(null);
+                    });
+                    that.parkingResults.reset();
+                    return;
+                }
+                // otherwise, just remove any markers not on the map anymore
+                var parkingResultsCopy = new Backbone.Collection(that.parkingResults.toJSON());
+                parkingResultsCopy.each(function(parkingResult) {
+                    var exists = false;
+                    for (var i = 0; i < results.length; i++) {
+                        if (result[i].coordinates.latitude === parkingResult.latitude &&
+                            result[i].coordinates.longitude === parkingResult.longitude &&
+                            result[i].location_name === parkingResult.name) {
+                            exists = true;
+                            break;
+                        }
+                    }
+                    if (!exists) {
+                        parkingResult.marker.setMap(null);
+                        that.parkingResults.remove(parkingResult);
+                    }
+                });
+                // make a new marker and add it to the map if it isn't there already
+                for (var i = 0; i < results.length; i++) {
+                    var exists = false;
+                    for (var j = 0; j < that.parkingResults.models.length; j++) {
+                        if (result[i].coordinates.latitude === that.parkingResults.models[j].latitude &&
+                            result[i].coordinates.longitude === that.parkingResults.models[j].longitude &&
+                            result[i].location_name === that.parkingResults.models[j].name) {
+                            exists = true;
+                            break;
+                        }
+                    }
+                    if (!exists) {
+                        var location = new google.maps.LatLng(results[i].coordinates.latitude,
+                            results[i].coordinates.longitude);
+                        var marker = new google.maps.Marker({
+                            map: BikeParking.map,
+                            icon: that.parkingIcon,
+                            title: results[i].location_name,
+                            position: location
+                        });
+                        var parkingResult = new ParkingResult({
+                            latitude: results[i].coordinates.latitude,
+                            longitude: results[i].coordinates.longitude,
+                            location: location,
+                            name: results[i].location_name,
+                            marker: marker
+                        });
+                    }
+                }
             });
         }, 1000)
 
