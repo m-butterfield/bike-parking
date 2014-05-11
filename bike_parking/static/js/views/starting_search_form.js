77d6af5377da80448541c29c58714f4eaa213aea
Matt Butterfield <mbutterfield@onshift.com>
2014-05-10 22:19:15 -0400

    reorganize fetchparkingresults

--- bike_parking/static/js/views/starting_search_form.js
+++ bike_parking/static/js/views/starting_search_form.js
@@ -38,20 +38,6 @@ define([
     BikeParking.searchBox = new google.maps.places.SearchBox($("#starting-address")[0]);
     BikeParking.boxVent = new Backbone.SearchBox({searchBox: BikeParking.searchBox});
 
-    var ParkingResult = Backbone.Model.extend({
-        initialize: function(options) {
-            this.latitude = options.latitude;
-            this.longitude = options.longitude;
-            this.location = options.location;
-            this.name = options.name;
-            this.marker = options.marker;
-        }
-    });
-
-    var ParkingResults = Backbone.Collection.extend({
-        model: ParkingResult
-    });
-
     var StartingSearchForm = Backbone.View.extend({
         events: {
             'submit': 'handleSubmit'
@@ -60,7 +46,6 @@ define([
         initialize: function() {
             this.listenTo(BikeParking.boxVent, 'box:places_changed', this.updateStartPoint);
             this.listenTo(BikeParking.mapVent, 'map:bounds_changed', this.updateSearchBounds);
-            this.listenTo(BikeParking.mapVent, 'map:idle', this.fetchParkingResults);
             this.startIcon = {
                 url: '/static/img/blue-bike.gif',
                 size: new google.maps.Size(70, 70),
@@ -68,14 +53,6 @@ define([
                 anchor: new google.maps.Point(35, 70),
                 scaledSize: new google.maps.Size(70, 70)
             };
-            this.parkingIcon = {
-                url: '/static/img/green-bike.gif',
-                size: new google.maps.Size(70, 70),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(35, 70),
-                scaledSize: new google.maps.Size(70, 70)
-            };
-            this.parkingResults = BikeParking.results = new ParkingResults();
         },
 
         updateSearchBounds: function() {
@@ -109,76 +86,6 @@ define([
             bounds.extend(startingPoint);
             BikeParking.map.fitBounds(bounds);
             BikeParking.map.setZoom(18);
-        },
-
-        fetchParkingResults: function() {
-            var bounds = BikeParking.map.getBounds();
-            var data = {
-                northeastLat: bounds.getNorthEast().lat(),
-                northeastLng: bounds.getNorthEast().lng(),
-                southwestLat: bounds.getSouthWest().lat(),
-                southwestLng: bounds.getSouthWest().lng()
-            }
-            var that = this;
-            $.ajax({
-                url: '/nearby_parking',
-                data: data
-            }).done(function(results) {
-                results = results.spots;
-                // if none were found, remove all from the map and collection
-                if (!results.length) {
-                    that.parkingResults.each(function(parkingResult) {
-                        parkingResult.marker.setMap(null);
-                    });
-                    that.parkingResults.reset();
-                    return;
-                }
-                // otherwise, just remove any markers not on the map anymore
-                var parkingResultsModels = that.parkingResults.models.slice();
-                parkingResultsModels.forEach(function(parkingResult) {
-                    var exists = false;
-                    for (var i = 0; i < results.length; i++) {
-                        if (results[i].lat === parkingResult.latitude &&
-                            results[i].lng === parkingResult.longitude) {
-                            exists = true;
-                            break;
-                        }
-                    }
-                    if (!exists) {
-                        parkingResult.marker.setMap(null);
-                        that.parkingResults.remove(parkingResult);
-                    }
-                });
-                // make a new marker and add it to the map if it isn't there already
-                for (var i = 0; i < results.length; i++) {
-                    var exists = false;
-                    for (var j = 0; j < that.parkingResults.models.length; j++) {
-                        if (results[i].lat === that.parkingResults.models[j].latitude &&
-                            results[i].lng === that.parkingResults.models[j].longitude) {
-                            exists = true;
-                            break;
-                        }
-                    }
-                    if (!exists) {
-                        var location = new google.maps.LatLng(results[i].lat,
-                            results[i].lng);
-                        var marker = new google.maps.Marker({
-                            map: BikeParking.map,
-                            icon: that.parkingIcon,
-                            title: results[i].name,
-                            position: location
-                        });
-                        var parkingResult = new ParkingResult({
-                            latitude: results[i].lat,
-                            longitude: results[i].lng,
-                            location: location,
-                            name: results[i].name,
-                            marker: marker
-                        });
-                        that.parkingResults.add(parkingResult);
-                    }
-                }
-            });
         }
 
     });
